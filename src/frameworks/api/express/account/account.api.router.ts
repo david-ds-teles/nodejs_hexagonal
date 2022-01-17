de12646b7204a6f306b4840f6e0017775e87ac58
David S Teles <94180051+david-ds-teles@users.noreply.github.com>
2022-01-17 13:07:34 -0300

    Mysql added
    * finished to implement mysql repository
    
    * mysql pm2 config

--- src/frameworks/api/express/account/account.api.router.ts
+++ src/frameworks/api/express/account/account.api.router.ts
@@ -3,12 +3,9 @@ import { AccountAPI } from '../../../../account/api/account.api';
 import { AccountService } from '../../../../account/core/services/account.service';
 import { IAccountService } from '../../../../account/core/services/iaccount.service';
 import { IAccountRepository } from '../../../../account/repository/iaccount.repository';
-import { IDBDriver } from '../../../../commons/idb.driver';
 import { IMessage } from '../../../../commons/imessage';
-import { MongoDBAccountRepository } from '../../../db/mongodb/mongodb.account.repository';
 
-export const accountRouter = <DB>(dbDriver: IDBDriver<DB>, message: IMessage): Router => {
-	const accountRepository: IAccountRepository = new MongoDBAccountRepository(dbDriver);
+export const accountRouter = (accountRepository: IAccountRepository, message: IMessage): Router => {
 	const accountService: IAccountService = new AccountService(accountRepository, message);
 	const accountApi = new AccountAPI(accountService);
 
